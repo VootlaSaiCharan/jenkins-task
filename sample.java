64eef945bf9da28c9e5c55f1929cf62d9b6680d1
vscharan <vscharan@gmail.com>
2023-11-03 15:51:14 +0530

    sample file uploaded

--- sample.java
+++ sample.java
@@ -1,9 +1,5 @@
 public class sample {
-	
-	public String name = "Tom";
-
-	public static void work() {
-		System.out.println("working");
-	}
-
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
